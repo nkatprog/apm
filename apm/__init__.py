4782de25bd0f6cf4aa86e41c9e6c4a2780e80d7a
Nik Kat <nkatprog@users.noreply.github.com>
2026-04-18 20:54:54 +0800

    chore: add author_email field and set default log level to DEBUG for local dev

--- apm/__init__.py
+++ apm/__init__.py
@@ -2,11 +2,20 @@
 
 Fork of microsoft/apm with extended capabilities for agentic workflows,
 authentication, and CLI logging.
+
+Personal fork: added __author_email__ and bumped default log level to DEBUG
+for easier local development tracing.
 """
 
 __version__ = "0.1.0"
 __author__ = "apm contributors"
+__author_email__ = ""
 __license__ = "MIT"
+
+import logging
+
+# Use DEBUG locally for easier tracing; change to WARNING in prod
+logging.getLogger("apm").setLevel(logging.DEBUG)
 
 from apm.core import App
 
